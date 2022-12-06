686af69707af355db21bfc1771f306d6587bfcc6
sirfelstein <117623418+sirfelstein@users.noreply.github.com>
2022-12-06 15:37:51 -0800

    Create node.h

---
+++ node.h
@@ -0,0 +1,21 @@
+/***********************************************************************************
+ * Program:node.h
+ * Author: Hyunjae Kim
+ * Date: 08/14/2020
+ * Description: This is header file of node
+ * ***********************************************************************************/
+
+
+
+#ifndef NODE_H
+#define NODE_H
+
+class Node{
+public:
+	int val; // the value that this node stores
+	Node *next; // a pointer to the next node in the list
+	// you can add constructors or other functionality if you find it useful or necessary
+	
+};
+
+#endif
